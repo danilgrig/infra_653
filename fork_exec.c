d142fac564d788019df411b69dbfe0cc78a80190
danilgrig <danilgrig@gmail.com>
2017-09-15 23:40:28 +0300

    add first and second programs

--- fork_exec.c
+++ fork_exec.c
@@ -5,17 +5,65 @@
 #include <string.h>
 #include <errno.h>
 
+//#define DEBUG_ON
+#if defined(DEBUG_ON)
+	#define DEBUG(format, args...) printf("LINE %d: " format, __LINE__, args)
+#else
+	#define DEBUG(format, args...) {}
+#endif
+
+#define MAX_WORDS 10
+#define MAX_CHARS 100
+
 int main() {
 	char **s;
-	s = (char**) malloc(10);
-	s[0] = (char*) malloc(100);
-	s[1] = (char*) malloc(100);
-	strcpy(s[0], "ls");
-	strcpy(s[1], "-al");
+	s = (char**) malloc(MAX_WORDS * sizeof(char*));
+	s[0] = (char*) malloc(MAX_CHARS);
+	char c;
+	int i = 0, j = 0, z;
+	pid_t p;
 
-	execvp(s[0], (char**)s);
-	printf("exec: %s\n", strerror(errno));
+	while (scanf("%c", &c) != EOF) {
+		DEBUG("c=%c\ti=%d\tj=%d\n", c, i, j);
+
+		if (c == '\n') {
+			p = fork();
+			if (p < 0) {
+				printf("can't fork\n");
+				exit(1);
+			} else if (p == 0) {
+				// child
+				s[j][i] = 0;
+				for (z = 0; z <= j; z++)
+					DEBUG("%s\n", s[z]);
+
+				s[j + 1] = NULL;
+				execvp(s[0], (char**)s);
+				printf("exec error: %s\n", strerror(errno));
+				exit(1);
+			} else {
+				//main process, free used memory
+				i = 0;
+				while (j > 0) {
+					free(s[j]);
+					j--;
+				}
+			}
+		} else if (c == ' ') {
+			s[j][i] = 0;
+			j++;
+			i = 0;
+			s[j] = (char*) malloc(MAX_CHARS);
+		} else {
+			s[j][i] = c;
+			i++;
+		}
+	}
+
+	while ((p = waitpid(-1, 0, 0)) > 0) {
+		printf("pid %d finished\n", p);
+	}
+	printf("all childs have finished, so i too\n");
 
 	return 0;
 }
-
---
+++ fork_pipe.c
@@ -0,0 +1,79 @@
+#include <sys/wait.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+
+//#define DEBUG_ON
+#if defined(DEBUG_ON)
+	#define DEBUG(format, args...) printf("LINE %d: " format, __LINE__, args)
+#else
+	#define DEBUG(format, args...) {}
+#endif
+
+
+int main(int argc, char **argv) {
+	if (argc < 4) {
+		printf("Usage: %s n l r\n", argv[0]);
+		exit(1);
+	}
+	int n = atoi(argv[1]);
+	int l = atoi(argv[2]);
+	int r = atoi(argv[3]);
+	int d = (r - l + n - 1) / n;
+	int *pipes = (int*)malloc(2 * n * sizeof(int));
+	int i = 0;
+	pid_t p;
+
+	while (l < r) {
+		if (pipe(&pipes[2 * i]) == -1) {
+			printf("pipe failed\n");
+			exit(1);
+		}
+		p = fork();
+		if (p < 0) {
+			printf("fork failed\n");
+			exit(1);
+		} else if (p == 0) {
+			int j;
+			for (j = 0; j <= 2 * i; j++)
+				close(pipes[j]);
+
+			long long s = 0;
+			for (j = l; j < r && j < (l + d); j++) {
+				s += j;
+			}
+			char buf[20];
+			sprintf(buf, "%lld", s);
+			write(pipes[2 * i + 1], buf, strlen(buf) + 1);
+			exit(0);
+		} else {
+			close(pipes[2 * i + 1]);
+			printf("pid %d started\n", p);
+		}
+
+		i++;
+		l += d;
+	}
+	if (i < n) {
+		printf("warn: seems n > r -l\n");
+		n = i;
+	}
+
+	while ((p = waitpid(-1, 0, 0)) > 0) {
+		printf("pid %d finished\n", p);
+	}
+
+	long long s = 0;
+	for (i = 0; i < n; i++) {
+		char buf[20];
+		int len;
+		len = read(pipes[2 * i], buf, 20);
+		buf[len] = 0;
+		s += atoll(buf);
+	}
+	printf("%lld\n", s);
+
+	return 0;
+}
