e7d6fdf7908b8069c066936d76d22c78c9d5216b
danil <danilgrig@gmail.com>
2018-03-03 15:51:34 +0300

    1

--- animal.cpp
+++ animal.cpp
@@ -4,30 +4,64 @@ using namespace std;
 
 class TAnimal {
 public:
-    void Say() {
-        cout << GetValue() << endl;
+    int mass, x;
+    TAnimal() {
+        mass = 10;
     }
-    virtual string GetValue() = 0;
+    void Say() {
+        cout << GetValue() << " " << mass << endl;
+    }
+    bool IsEnoughScores() {
+        return 1 > 2;
+    }
+    virtual string GetValue() const = 0;
 };
 
 class TCat : public TAnimal {
 public:
-    string GetValue() override {
-        return "Cat";
+    string GetValue() const override  {
+        string s = "Cat";
+        return s + s;
     }
 };
 
 class TDog : public TAnimal {
 public:
-    string GetValue() override {
+    string GetValue() const override {
         return "Dog";
     }
 };
 
+class NoValue : public exception {
+public:
+    string what() {
+        return "No such value";
+    }
+};
+
+class UnknownAnimal : public exception {
+public:
+    string what() {
+        return "UnknownAnimal";
+    }
+};
+
+TAnimal* GetAnimal(const string& type) {
+    if (type == "Cat") {
+        return new TCat();
+    } else if (type == "Dog") {
+        return new TDog();
+    } else {
+        throw UnknownAnimal();
+    }
+}
+
 int main() {
-    int *x = new int;
-    free(x);
-    free(x);
-    TCat c;
-    c.Say();
+    TAnimal *zoo[10];
+    zoo[0] = GetAnimal("Cat");
+    zoo[1] = GetAnimal("Dog");
+
+    for (int i = 0; i < 2; i++) {
+        zoo[i]->Say();
+    }
 }
